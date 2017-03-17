a5f145269e9291ea05a33ae148c902b65b768189
Shruti Priya <shrutipriya1808@gmail.com>
2017-03-17 11:21:02 -0700

    got the updated key

--- js/doctor.js
+++ js/doctor.js
@@ -8,7 +8,7 @@ DoctorData.prototype.getDoctors = function(medicalIssue, displayDoctors){
 
   var doctorDataObj = this;
 
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=1&user_key=' + apiKey)
   .then(function(result) {
     result.data.forEach(function(dataEntry){
       DoctorDataObj.retrieved.push(dataEntry);
